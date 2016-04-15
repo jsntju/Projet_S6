7383918b28fef21803750afa375262b946eab488
jsntju <jsnt.denis@gmail.com>
2016-04-15 09:53:47 +0200

    Update and rename LEDs.c to LEDs_PAD.c

--- LEDs_PAD.c
+++ LEDs_PAD.c
@@ -1,4 +1,10 @@
-#include <__cross_studio_io.h> 
+/*
+* @file: Leds_pad.c 
+* @Brief: Fonctions permettant d'allumer la LED correspondant à la direction du PAD (haut, bas, droite, gauche, centre).
+*/
+
+
+#include <__cross_studio_io.h>        //Pour le printf
 #include  <msp430x16x.h> 
 
  
@@ -7,81 +13,50 @@
 void main(void){  
 
   WDTCTL = WDTPW + WDTHOLD;             // Stop watchdog timer 
-
-  P1DIR |= 0x1F;                        // Set P1.0 to output direction 
-
-  P2DIR &= 0xE0; 
-
-  P1OUT &= 0xE0; 
+  P1DIR |= 0x1F;                        // P1 mis en sortie
+  P2DIR &= 0xE0;                        // P2 mis en entrée
+  P1OUT &= 0xE0;                        // Initialise la sortie de P1 à 0
 
    
 
   for (;;){ 
-
       unsigned int i; 
 
-      debug_printf("%x\n",~P2IN); 
-
-   
-
-      //P1OUT = ~P2IN;                      // Toggle P1.0 using exclusive-OR 
-
+      //debug_printf("%x\n",~P2IN); 
+      //P1OUT = ~P2IN;                      // Led en fonction de la sortie de P2
       /* 
-
       i = 50000;                          // Delay 
-
       do (i--); 
-
       while (i != 0);*/ 
 
    
 
       switch(~P2IN&0x0000001F){ 
-
           case (0x01): 
-
               P1OUT &= 0xE0; 
-
               P1OUT |= 0x01; 
-
               break; 
 
           case (0x02): 
-
               P1OUT &= 0xE0; 
-
               P1OUT |= 0x04; 
-
               break; 
 
           case (0x04): 
-
               P1OUT &= 0xE0; 
-
               P1OUT |= 0x02; 
-
               break; 
 
           case (0x08): 
-
               P1OUT &= 0xE0; 
-
               P1OUT |= 0x10; 
-
               break; 
 
           case (0x10): 
-
               P1OUT &= 0xE0; 
-
               P1OUT |= 0x08; 
-
               break; 
-
       } 
-
   } 
-
   //debug_exit(0); 
-
 } 
